64b425afe64964d1fc42dbc88eef9e0d8e31f44e
Abhiram Gunisetty <abhi.gunisetti@gmail.com>
2022-01-19 15:28:32 +1030

    latest

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,18 +1,31 @@
-import React from 'react'
-import "./home.css"
-import Header from "../../components/header/Header.jsx"
-import Sidebar from "../../components/sidebar/Sidebar"
-import Posts from "../../components/posts/Posts"
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router";
+import Header from "../../components/header/Header";
+import Posts from "../../components/posts/Posts";
+import Sidebar from "../../components/sidebar/Sidebar";
+import "./home.css";
+import axios from "axios";
 
 export default function Home() {
-    return (
-        <>
-            <Header />
-        <div className="home" >
-            <Posts/>
-            <Sidebar />
-            
-        </div>
-        </>
-    )
+  // const location = useLocation();
+  // console.log(location);
+
+  const [posts, setPosts]  = useState([]);
+
+  useEffect(()=> {
+    const fetchPost = async () => {
+      const res = await axios.get("/posts")
+      console.log("res is",res);
+    }
+    fetchPost();
+  }, [])
+  return (
+    <>
+      <Header />
+      <div className="home">
+        <Posts />
+        <Sidebar />
+      </div>
+    </>
+  );
 }
